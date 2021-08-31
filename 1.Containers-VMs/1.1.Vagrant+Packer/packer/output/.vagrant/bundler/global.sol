b31a7b979608bc2be1f6aee2f4c146c0a18d70cf
root <root@master-1.egor.kupchik>
2021-08-31 13:28:09 +0300

    update file1

---
+++ 1.Containers-VMs/1.1.Vagrant+Packer/packer/output/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-omnibus",["= 1.5.0"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.30.0"]]],"checksum":"1d7294ee07237a011643dae4a55c3472b33e99c359022d4c9ad83ec95b2a66d2","vagrant_version":"2.2.9"}
